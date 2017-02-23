830c0e1d01c04f47b50213c6e415eb4bb29f8037
Raju Bibar <rbibar@cisco.com>
2017-02-23 22:00:06 +0530

    Added more java file

--- CustomCache/src/com/java/cache/CacheManager.java
+++ CustomCache/src/com/java/cache/CacheManager.java
@@ -18,39 +18,31 @@ public class CacheManager {
     {
         try
         {
-            /* Create background thread, which will be responsible for
-purging expired items. */
+            /* Create background thread, which will be responsible forpurging expired items. */
             Thread threadCleanerUpper = new Thread(
             new Runnable()
             {
-              /*  The default time the thread should sleep between scans.
-                  The sleep method takes in a millisecond value so 5000 = 5
-Seconds.
+              /*  The default time the thread should sleep between scans. The sleep method takes in a millisecond value so 5000 = 5 Seconds.
               */
               int milliSecondSleepTime = 5000;
               public void run()
               {
                 try
                 {
-                  /* Sets up an infinite loop.  The thread will continue
-looping forever. */
+                  /* Sets up an infinite loop.  The thread will continue looping forever. */
                   while (true)
                   {
                     System.out.println("ThreadCleanerUpper Scanning For Expired Objects...");
-                    /* Get the set of all keys that are in cache.  These are
-the unique identifiers */
+                    /* Get the set of all keys that are in cache.  These are the unique identifiers */
                     java.util.Set keySet = cacheHashMap.keySet();
                     /* An iterator is used to move through the Keyset */
                     java.util.Iterator keys = keySet.iterator();
-                    /* Sets up a loop that will iterate through each key in
-the KeySet */
+                    /* Sets up a loop that will iterate through each key in the KeySet */
                     while(keys.hasNext())
                     {
-                      /* Get the individual key.  We need to hold on to this
-key in case it needs to be removed */
+                      /* Get the individual key.  We need to hold on to this key in case it needs to be removed */
                       Object key = keys.next();
-                      /* Get the cacheable object associated with the key
-inside the cache */
+                      /* Get the cacheable object associated with the key inside the cache */
                       Cacheable value = (Cacheable)cacheHashMap.get(key);
                       /* Is the cacheable object expired? */
                       if (value.isExpired())
@@ -67,8 +59,7 @@ inside the cache */
              *****
 ************************************************************************
                     */
-                    /* Puts the thread to sleep.  Don't need to check it
-continuously */
+                    /* Puts the thread to sleep.  Don't need to check it continuously */
                     Thread.sleep(this.milliSecondSleepTime);
                   }
                 }
